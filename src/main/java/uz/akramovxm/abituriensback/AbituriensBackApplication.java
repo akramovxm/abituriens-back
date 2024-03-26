baab12d7194725c934d00cda5ee90c7b60c6b338
Khojiakbar <khojiakbar@Khojiakbars-MacBook-Air.local>
2024-03-26 21:34:29 +0500

    initial commit

---
+++ src/main/java/uz/akramovxm/abituriensback/AbituriensBackApplication.java
@@ -0,0 +1,13 @@
+package uz.akramovxm.abituriensback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbituriensBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbituriensBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/akramovxm/abituriensback/AbituriensBackApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.akramovxm.abituriensback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbituriensBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
